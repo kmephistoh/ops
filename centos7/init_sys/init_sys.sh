23dd3fcbddbd5abc7c0542f22e4cca5f3ecd67d5
阚浩 <kanhao@chelun.com>
2015-09-23 14:25:57 +0800

    去掉nginx版本信息

--- centos7/init_sys/init_sys.sh
+++ centos7/init_sys/init_sys.sh
@@ -1,9 +1,10 @@
 #/bin/sh
 #os version: CentOS-7-x86_64-Minimal-1503-01.iso
 
-# 0. change repo to 163
+# 0. change repo to aliyun
 mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
-curl -0 /etc/yum.repos.d/CentOS7-Base-163.repo  http://mirrors.163.com/.help/CentOS7-Base-163.repo
+curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+yum makecache
 
 # 1. update
 yum -y update
@@ -19,4 +20,13 @@ timedatectl set-timezone Asia/Shanghai
 sed -i '/SELINUX/s/enforcing/disabled/' /etc/selinux/config 
 
 # 4. sys tools
-yum install -y vim strace telnet lsof
+yum install -y vim strace telnet lsof wget
+
+# 5. change ssh port
+yum -y install policycoreutils-python
+semanage port -a -t ssh_port_t -p tcp 3520
+cp -p /etc/ssh/sshd_config /etc/ssh/sshd_config.orig.$(date +%F)
+sed -i 's/^#Port 22/Port 3520/g' /etc/ssh/sshd_config
+firewall-cmd --permanent --zone=public --add-port=3520/tcp
+firewall-cmd --reload
+systemctl restart sshd.service
